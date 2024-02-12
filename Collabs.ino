b1983d143fc03923d342ccec8a738750a64b2da9
Rizki Saputra Sembiring <rizkisaputrasembiring.rss@gmail.com>
2024-02-12 09:28:05 +0700

    Automatic Sleeve Placket

--- Collabs.ino
+++ Collabs.ino
@@ -1,3 +1,4 @@
+// inisialisasi pin, ditambahkan ketika ada pin input/output (IO) yang digunakan
 int R1 = D13;
 int R2 = D12;
 int R3 = D11;
@@ -7,10 +8,15 @@ int menu, kondisi = 0;
 void setup() {
   Serial.begin(115200);
   menu = 0;
+  //pinMode untuk input (push button, sensor, dll)
+  //INPUT_PULLUP digunakan untuk tombol, dan beberapa sensor analog
   pinMode(inputPin, INPUT_PULLUP);
+  //pinMode untuk output (motor, relay, dll)
   pinMode(R1, OUTPUT);
   pinMode(R2, OUTPUT);
   pinMode(R3, OUTPUT);
+
+  //relay harus keadaan high terlebih dahulu sebelum ke void loop
   digitalWrite(R1, HIGH);
   digitalWrite(R2, HIGH);
   digitalWrite(R3, HIGH);
@@ -18,8 +24,10 @@ void setup() {
 }
 
 void loop() {
+  //ketika tombol/sensor terdeteksi maka menu akan bertambah 1, menggunakan fungsi button debounce
   if (digitalRead(inputPin) == LOW && kondisi == 1) {
     menu++;
+    //berfungsi untuk membatasi menu sehingga menu akan kembali ke 0 
     if (menu >= 4) {
       menu = 0;
     }
@@ -37,6 +45,8 @@ void loop() {
 }
 
 void pneumatic(){
+  //menu pada void loop akan dibaca sebagai switch case, sehingga ketika menu = 1 maka case = 1
+  //pada bagian ini misal menu 1 menghidupkan relay 1, maka case 1 berisi digitalWrite(R1, LOW);
     switch (menu) {
       case 0:
         digitalWrite(R1, HIGH);
---
+++ Develop/Automatic_Sleeve_Placket/Automatic_Sleeve_Placket.ino
@@ -0,0 +1,92 @@
+// inisialisasi pin, ditambahkan ketika ada pin input/output (IO) yang digunakan
+int R1 = 2;
+int R2 = 3;
+int R3 = 4;
+int R4 = 5;
+int R5 = 6;
+int R6 = 7;
+int R7 = 8;
+int R8 = 9;
+int R9 = 10;
+int R10 = 11;
+int inputPin = 13;
+int menu, kondisi = 0;
+
+void setup() {
+  Serial.begin(115200);
+  menu = 0;
+  //pinMode untuk input (push button, sensor, dll)
+  //INPUT_PULLUP digunakan untuk tombol, dan beberapa sensor analog
+  pinMode(inputPin, INPUT_PULLUP);
+  for (int pin = 2; pin <= 7; pin++) {
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, HIGH);
+  }
+  digitalWrite(7, LOW);
+  delay(750);
+  //pinMode untuk output (motor, relay, dll)
+  for (int pin = 2; pin <= 5; pin++) {
+    //    pinMode(pin, OUTPUT);
+    delay(250);
+    digitalWrite(pin, LOW);
+    //    digitalWrite(pin, HIGH);
+    Serial.println(pin);
+  }
+  delay(1000);
+  digitalWrite(7, HIGH);
+  //  for(int tes = 2; tes<=5; tes++){
+  //    digitalWrite(tes, LOW);
+  //  }
+  //  delay(5000);
+  //  for(int tes = 2; tes<=6; tes++){
+  //    digitalWrite(tes, HIGH);
+  //  }
+  Serial.println("Starting");
+}
+
+void loop() {
+  if (digitalRead(inputPin) == HIGH) {
+    for (int tes = 2; tes <= 5; tes++) {
+          digitalWrite(tes, HIGH);
+        }
+  }
+  //ketika tombol/sensor terdeteksi maka menu akan bertambah 1, menggunakan fungsi button debounce
+  //  if (digitalRead(inputPin) == LOW && kondisi == 1) {
+  //    menu++;
+  //    //berfungsi untuk membatasi menu sehingga menu akan kembali ke 0
+  //    if (menu >= 4) {
+  //      menu = 0;
+  //    }
+  //    Serial.println(menu);
+  //    Serial.println("HIGH");
+  //    kondisi = 0;
+  //    Serial.println("Delay");
+  //    pneumatic();
+  //    delay(1000);
+  //  }
+  //  if (digitalRead(inputPin) == HIGH && kondisi == 0) {
+  //    Serial.println("LOW");
+  //    kondisi = 1;
+  //  }
+}
+
+void pneumatic() {
+  //menu pada void loop akan dibaca sebagai switch case, sehingga ketika menu = 1 maka case = 1
+  //pada bagian ini misal menu 1 menghidupkan relay 1, maka case 1 berisi digitalWrite(R1, LOW);
+  switch (menu) {
+    case 0:
+      digitalWrite(R1, HIGH);
+      digitalWrite(R2, HIGH);
+      digitalWrite(R3, HIGH);
+      break;
+    case 1:
+      digitalWrite(R1, LOW);
+      break;
+    case 2:
+      digitalWrite(R1, HIGH);
+      break;
+    case 3:
+      digitalWrite(R2, LOW);
+      break;
+  }
+}
