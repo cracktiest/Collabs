281b18a4ef84314aaefa446c191fa16d387735f1
Rizki Saputra Sembiring <rizkisaputrasembiring.rss@gmail.com>
2024-07-26 08:17:29 +0700

    sketch has been added
    include .elf and .hex

---
+++ sketch/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -0,0 +1,59 @@
+# 1 "C:\\Users\\PC-P005\\Documents\\Arduino\\Collabs\\Develop\\SPP\\SPP.ino"
+int menu, a, b, c, d, e, f = 0;
+
+void setup()
+{
+    for (int bootInput = 2; bootInput <= 3; bootInput++)
+    {
+        pinMode(bootInput, 0x2);
+    }
+    for (int bootOutput = 4; bootOutput <= 9; bootOutput++)
+    {
+        pinMode(bootOutput, 0x1);
+        digitalWrite(bootOutput, 0x0);
+        delay(500);
+        digitalWrite(bootOutput, 0x1);
+        delay(1000);
+    }
+}
+void loop()
+{
+    if (digitalRead(2) == 0x1)
+    {
+        menu++;
+        if (menu > 4)
+        {
+            menu = 0;
+        }
+    }
+    pneumatic();
+}
+
+void pneumatic()
+{
+    switch (menu)
+    {
+    case 0:
+        for (int i = 4; i <= 9; i++)
+        {
+            digitalWrite(i, 0x1);
+            delay(50);
+        }
+        break;
+    case 1:
+        digitalWrite(2, 0x0);
+        delay(3000);
+        digitalWrite(3, 0x0);
+        delay(5000);
+        break;
+    case 2:
+        digitalWrite(3, 0x1);
+        delay(3000);
+        digitalWrite(2, 0x1);
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    }
+}
---
+++ sketch/sketch/SPP.ino.cpp
@@ -0,0 +1,67 @@
+#include <Arduino.h>
+#line 1 "C:\\Users\\PC-P005\\Documents\\Arduino\\Collabs\\Develop\\SPP\\SPP.ino"
+int menu, a, b, c, d, e, f = 0;
+
+#line 3 "C:\\Users\\PC-P005\\Documents\\Arduino\\Collabs\\Develop\\SPP\\SPP.ino"
+void setup();
+#line 18 "C:\\Users\\PC-P005\\Documents\\Arduino\\Collabs\\Develop\\SPP\\SPP.ino"
+void loop();
+#line 31 "C:\\Users\\PC-P005\\Documents\\Arduino\\Collabs\\Develop\\SPP\\SPP.ino"
+void pneumatic();
+#line 3 "C:\\Users\\PC-P005\\Documents\\Arduino\\Collabs\\Develop\\SPP\\SPP.ino"
+void setup()
+{
+    for (int bootInput = 2; bootInput <= 3; bootInput++)
+    {
+        pinMode(bootInput, INPUT_PULLUP);
+    }
+    for (int bootOutput = 4; bootOutput <= 9; bootOutput++)
+    {
+        pinMode(bootOutput, OUTPUT);
+        digitalWrite(bootOutput, LOW);
+        delay(500);
+        digitalWrite(bootOutput, HIGH);
+        delay(1000);
+    }
+}
+void loop()
+{
+    if (digitalRead(2) == HIGH)
+    {
+        menu++;
+        if (menu > 4)
+        {
+            menu = 0;
+        }
+    }
+    pneumatic();
+}
+
+void pneumatic()
+{
+    switch (menu)
+    {
+    case 0:
+        for (int i = 4; i <= 9; i++)
+        {
+            digitalWrite(i, HIGH);
+            delay(50);
+        }
+        break;
+    case 1:
+        digitalWrite(2, LOW);
+        delay(3000);
+        digitalWrite(3, LOW);
+        delay(5000);
+        break;
+    case 2:
+        digitalWrite(3, HIGH);
+        delay(3000);
+        digitalWrite(2, HIGH);
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    }
+}
